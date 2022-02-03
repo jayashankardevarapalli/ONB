bc82f0762c052729248b8f47c9631ddaaa37f71d
jayashankardevarapalli <devarapallijayashankarkumar@gmail.com>
2022-02-03 10:40:59 +0530

    Index

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,14 @@
-from django.urls import path,include
-from .views import index,dashboard,about,delnotes,notes,todo,deltodo
+from django.urls import path, include
+from .views import index, dashboard, about, delnotes, notes, todo, deltodo, signin, signup
 
 urlpatterns = [
-	path('', index, name='index'),
-	path('dashboard/', dashboard, name='dashboard'),
-	path('todo/', todo, name='todo'),
-	path('delnotes/<int:noteid>', delnotes, name='delnotes'),
-	path('deltodo/<int:todoid>', deltodo, name='deltodo'),
-	path('notes/', notes, name='notes'),
-	path('about/', about, name='about')
+    path('', index, name='index'),
+    path('signin/', signin, name='signin'),
+    path('signup/', signup, name='signup'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('todo/', todo, name='todo'),
+    path('delnotes/<int:noteid>', delnotes, name='delnotes'),
+    path('deltodo/<int:todoid>', deltodo, name='deltodo'),
+    path('notes/', notes, name='notes'),
+    path('about/', about, name='about')
 ]
--- app/views.py
+++ app/views.py
@@ -1,105 +1,108 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Notes,Todo
-
+from .models import Notes, Todo
 
 
 def index(request):
-	return HttpResponse("Index")
+    return render(request, 'index.html')
+
+
+def signin(request):
+    return render(request, 'signin.html')
+
+
+def signup(request):
+    return render(request, 'signup.html')
+
 
 def about(request):
-	return render(request, 'about.html')
+    return render(request, 'about.html')
+
 
 def dashboard(request):
-	noteid = int(request.GET.get('noteid', 0))
-	note = Notes.objects.all()
-	
+    noteid = int(request.GET.get('noteid', 0))
+    note = Notes.objects.all()
 
-	if request.method == 'POST':
-		noteid =  int(request.POST.get('noteid', 0))
-		title = request.POST.get('title')
-		content = request.POST.get('content', '')
+    if request.method == 'POST':
+        noteid = int(request.POST.get('noteid', 0))
+        title = request.POST.get('title')
+        content = request.POST.get('content', '')
 
-		if noteid > 0:
-			notes = Notes.objects.get(pk=noteid)
-			notes.title = title
-			notes.content = content
-			notes.save()
+        if noteid > 0:
+            notes = Notes.objects.get(pk=noteid)
+            notes.title = title
+            notes.content = content
+            notes.save()
 
-			return redirect('/dashboard/?noteid=%i' % noteid)
-		else:
-			notes = Notes.objects.create(title=title, content=content)
-			
-			return redirect('/dashboard/?noteid=%i' % notes.id)
+            return redirect('/dashboard/?noteid=%i' % noteid)
+        else:
+            notes = Notes.objects.create(title=title, content=content)
 
+            return redirect('/dashboard/?noteid=%i' % notes.id)
 
-	if noteid > 0:
-		notes = Notes.objects.get(pk=noteid)
-	else:
-		notes = ''
+    if noteid > 0:
+        notes = Notes.objects.get(pk=noteid)
+    else:
+        notes = ''
 
+    data = {
+        'noteid': noteid,
+        'note': note,
+        'notes': notes
+    }
 
-
-	data = {
-			'noteid': noteid,
-			'note': note,
-			'notes': notes
-	}
-
-	return render(request, 'dashboard.html', data)
+    return render(request, 'dashboard.html', data)
 
 
 def todo(request):
-	todoid = int(request.GET.get('todoid', 0))
-	todos = Todo.objects.all()
+    todoid = int(request.GET.get('todoid', 0))
+    todos = Todo.objects.all()
 
+    if request.method == 'POST':
+        todoid = int(request.POST.get('todoid', 0))
+        title = request.POST.get('title')
 
-	if request.method == 'POST':
-		todoid = int(request.POST.get('todoid', 0))
-		title = request.POST.get('title')
+        if todoid > 0:
+            tod = Todo.objects.get(pk=todoid)
+            tod.todo = todo
+            tod.save()
 
-		if todoid > 0:
-			tod = Todo.objects.get(pk=todoid)
-			tod.todo = todo
-			tod.save()
+            return redirect('/todo/?todoid=%i' % todoid)
 
-			return redirect('/todo/?todoid=%i' % todoid)
+        else:
+            tod = Todo.objects.create(title=title)
 
-		else:
-			tod = Todo.objects.create(title=title)
+            return redirect('/todo/?todoid=%i' % tod.id)
 
-			return redirect('/todo/?todoid=%i' % tod.id)
+    if todoid > 0:
+        tod = Todo.objects.get(pk=todoid)
+    else:
+        tod = ''
 
-	if todoid > 0:
-		tod = Todo.objects.get(pk=todoid)
-	else:
-		tod = ''
+    data = {
+        'todoid': todoid,
+        'todos': todos,
+        'tod': tod
+    }
 
-	data = {
-			'todoid': todoid,
-			'todos': todos,
-			'tod': tod
-	}
+    return render(request, 'todo.html', data)
 
-	return render(request, 'todo.html', data)
 
 def delnotes(request, noteid):
-	notes = Notes.objects.get(pk=noteid)
-	notes.delete()
+    notes = Notes.objects.get(pk=noteid)
+    notes.delete()
 
-	return redirect('/dashboard/?noteid=0')
+    return redirect('/dashboard/?noteid=0')
+
 
 def deltodo(request, todoid):
-	tod = Todo.objects.get(pk=todoid)
-	tod.delete()
+    tod = Todo.objects.get(pk=todoid)
+    tod.delete()
 
-	return redirect('/todo/?todoid=0')
+    return redirect('/todo/?todoid=0')
+
 
 def notes(request):
-	note = Notes.objects.all()[:11]
-	
-	return render(request, 'notes.html', {'note':note})
+    note = Notes.objects.all()[:11]
 
-
-
-
+    return render(request, 'notes.html', {'note': note})
