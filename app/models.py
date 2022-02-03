5ca7613deffbfd0f8864c6a05d254bc1fde1dc26
jayashankardevarapalli <devarapallijayashankarkumar@gmail.com>
2022-02-03 15:15:38 +0530

    Base Completed

--- app/models.py
+++ app/models.py
@@ -1,23 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Notes(models.Model):
-	title = models.CharField(max_length=250)
-	content = models.TextField(null=True)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=250)
+    content = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-	class Meta:
-		ordering = ['-created_at']
+    class Meta:
+        ordering = ['-created_at']
 
-		def __str__(self):
-			return self.title
+        def __str__(self):
+            return self.title
 
 
 class Todo(models.Model):
-	title = models.CharField(max_length=1000)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
